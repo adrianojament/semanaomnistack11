b7119af6517dc38ce364cf4f0d3047142cbcd66e
Adriano Ament <adrianojament@gmail.com>
2020-03-28 12:15:12 -0300

    front end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Routes from "./routes"
+import './global.css'
 
 function App() {
+  // const [counter, setCounter] = useState(0);
+  // o useState retorna um Array;
+  // Array [valor, FuncaodeAtualizacao]
+  // function increment(){
+  // setCounter(counter + 1);    
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmmiStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes/>
     </div>
   );
 }
