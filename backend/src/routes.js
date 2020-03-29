eb44422a59a91d9e3869b32b5939adb78781ce5d
Adriano Ament <adrianojament@gmail.com>
2020-03-29 14:16:44 -0300

    Adicionando os testes.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,31 +1,73 @@
 const express = require("express");
-const ongController = require('./controllers/OngController');
-const incendentController = require('./controllers/IncidentController');
-const profileController = require('./controllers/ProfileController');
-const sessionControler = require('./controllers/SessionController');
+const { celebrate, Segments, Joi } = require("celebrate");
+
+const ongController = require("./controllers/OngController");
+const incendentController = require("./controllers/IncidentController");
+const profileController = require("./controllers/ProfileController");
+const sessionControler = require("./controllers/SessionController");
 
 const routes = express.Router();
 
 routes.post("/sessions", sessionControler.create);
 
 routes.get("/ongs", ongController.index);
-routes.post("/ongs", ongController.create);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.string()
+        .required()
+        .min(10)
+        .min(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  ongController.create
+);
 
-routes.get("/profile", profileController.index);
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi
+      .object({
+        authorization: Joi.string().required()
+      }).unknown()
+  }),
+  profileController.index
+);
 
 routes.post("/incidents", incendentController.create);
-routes.get("/incidents", incendentController.index);
-routes.delete("/incidents/:id", incendentController.delete);
+routes.get("/incidents", celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    }),
+}), incendentController.index);
 
+
+routes.delete("/incidents/:id", celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    }),
+    [Segments.HEADERS]: Joi
+      .object({
+        authorization: Joi.string().required()
+      }).unknown()
+}) ,incendentController.delete);
 
 module.exports = routes;
-
 
 /**
  * Rota/ Recurso
  */
 
- /*
+/*
     * Metodos HTTP:
     
     * GET: Buscar as informações do back-end
@@ -34,13 +76,13 @@ module.exports = routes;
     * DELETE: Deleta uma informação no back-end
  */
 
- /**
-  * Tipos de Parametros:
-  * 
-  * Query Params: Parametros enviados na rota apos o simbolo de "?", eles servem para filtros e paginação;
-  * Route Params: Parametros utilizados para identificar recursos;
-  * Request Body: Corpo da requisão, utilizado para criar / alterar os dados
-  */
+/**
+ * Tipos de Parametros:
+ *
+ * Query Params: Parametros enviados na rota apos o simbolo de "?", eles servem para filtros e paginação;
+ * Route Params: Parametros utilizados para identificar recursos;
+ * Request Body: Corpo da requisão, utilizado para criar / alterar os dados
+ */
 
 /*
  routes.post("/ongs", async (request, response) => {
@@ -66,4 +108,3 @@ module.exports = routes;
     return response.json({id});
 });
 */
-
